3a739de35d8ef704334da253d17f9ebbc68c6bcb
Felipe Sembay <felipesembay91@gmail.com>
2022-12-26 01:47:30 +0000

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# backtest
+# Backtest Trading!
+
+O Backtest Trading é um aplicativo web para fazer backtest de estratégias de trade. 
+
+Nesse projeto estamos usando algumas bibliotecas como: **VectorBT, Yfinance, Plotly, Streamlit, Numpy, Pandas e etc.**
--- backtest.py
+++ backtest.py
@@ -6,7 +6,34 @@ import warnings
 warnings.filterwarnings('ignore')
 import yfinance as yf
 import plotly.graph_objs as go
+import time
+from datetime import date, datetime
 
 st.title('WebApp to make estrategy backtest trading!')
 
-@st.cache
+ticker = st.text_input('Enter the ticker symbol of the stock you want to backtest')
+periodo = st.date_input('Periodo', date(2022, 1, 1), min_value=date(2000, 1, 1), max_value=datetime.now())
+interval = st.selectbox('Intervalo', ['1m', '2m', '5m', '15m', '30m','1h', '2h', '4h', '6h', '8h', '12h', '1d','2d' ,'3d', '1wk', '1mo','3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'])
+
+if st.button('Get Data'):
+    st.write('Getting data...')
+    data = vbt.YFData.download(ticker, periodo, interval=interval).get()
+    st.write('Data downloaded!')
+    st.write(data)
+
+prices = data['Close']
+volume = data['Volume']
+Indicators = st.selectbox('Indicadores', ['MACD', 'RSI', 'Stochastic', 'MA'])
+mr = st.slider('Media Rapida', 0, 200, 50)
+ml = st.slider('Media Lenta', 0, 200, 50)
+
+media_rapida = vbt.Indicators.run(princes, mr)
+media_lenta = vbt.Indicators.run(princes, ml)
+
+lower = Indicators.lower()
+entrada = media_rapida.{lower}_above(media_lenta)
+saida = media_rapida.{lower}_below(media_lenta)
+
+pf_macd = vbt.Portfolio.from_signals(prices, entradas_macd, saidas_macd)
+
+pf_macd = vbt.Portfolio.from_signals(prices, entradas_macd, saidas_macd)
